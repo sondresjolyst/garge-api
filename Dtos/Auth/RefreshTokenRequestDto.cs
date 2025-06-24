cedf629fdeee6792ddb2e8f9b3d3cb97a188e242
Sondre Sjølyst <sonyslyst@gmail.com>
2025-06-24 17:00:17 +0200

    feat: refresh token (#16)

---
+++ Dtos/Auth/RefreshTokenRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace garge_api.Dtos.Auth
+{
+    public class RefreshTokenRequestDto
+    {
+        public required string Token { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
