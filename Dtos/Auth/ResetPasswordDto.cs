5ccc60af14894907c0acdee7524a784832bdb570
Sondre Sjølyst <sonyslyst@gmail.com>
2025-06-24 17:23:24 +0200

    feat: reset password (#19)

---
+++ Dtos/Auth/ResetPasswordDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace garge_api.Dtos.Auth
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        public required string Email { get; set; }
+        [Required]
+        public required string Code { get; set; }
+        [Required]
+        public required string NewPassword { get; set; }
+    }
+}
