2bdf8d2a84db5065ae63a63b68b840d72d7dc995
Sondre Sjølyst <sonyslyst@gmail.com>
2025-08-09 18:28:12 +0200

    feat: automation (#26)

---
+++ Dtos/Automation/AutomationRuleDto.cs
@@ -0,0 +1,14 @@
+﻿namespace garge_api.Dtos.Automation
+{
+    public class AutomationRuleDto
+    {
+        public int Id { get; set; }
+        public required string TargetType { get; set; }
+        public int TargetId { get; set; }
+        public required string SensorType { get; set; }
+        public int SensorId { get; set; }
+        public required string Condition { get; set; }
+        public double Threshold { get; set; }
+        public required string Action { get; set; }
+    }
+}
